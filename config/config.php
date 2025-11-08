4d6b22a624f92170c965d1e9b90adebf2e68408a
phamkhang21032004z-dot <phamkhang21032004z@gmail.com>
2025-11-07 22:40:51 -0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,13 +1,13 @@
 <?php
 $configDB = array();
-$configDB["host"] 		= "hktsql.mysql.database.azure.com";
+$configDB["host"] 		= "localhost";
 $configDB["database"]	= "computer_store";
 $configDB["username"] 	= "khang";
-$configDB["password"] 	= "tdc@12345";
-define("HOST", "hktsql.mysql.database.azure.com");
+$configDB["password"] 	= "tdc@123";
+define("HOST", "localhost");
 define("DB_NAME", "computer_store");
 define("DB_USER", "khang");
-define("DB_PASS", "tdc@12345");
+define("DB_PASS", "tdc@123");
 define('ROOT', dirname(dirname(__FILE__) ) );
 //Thu muc tuyet doi truoc cua config; c:/wamp/www/lab/
 define("BASE_URL", "http://".$_SERVER['SERVER_NAME']);//dia chi website
